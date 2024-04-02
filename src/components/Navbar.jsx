9bf63ac0b571c71577c74d0d8ac93606a94d0fc7
moshudougee <mbuthiadenis@gmail.com>
2024-04-02 19:27:00 +0300

    edit sidebar nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -152,7 +152,7 @@ const Navbar = () => {
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
                     <a
                     className='flex justify-between items-center w-full text-gray-300'
-                    href="/docs/FULL STACK RESUME.docx" 
+                    href="/docs/full-stack resume.pdf" 
                     target="_blank"
                     rel='noreferrer' 
                     download="My Resume"
