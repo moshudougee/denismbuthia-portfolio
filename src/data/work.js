1fc13336030c7f0913f7fd7b26bbb6877392cfa0
moshudougee <mbuthiadenis@gmail.com>
2024-04-19 01:51:34 +0300

    add dembu-estate

--- src/data/work.js
+++ src/data/work.js
@@ -1,50 +1,7 @@
 import Img1 from '../assets/projects/dembu-social2.png';
 import Img2 from '../assets/projects/dembublog.png';
+import Img3 from '../assets/projects/dembu-estate.png';
 
-/** 
-export const workData = [
-    {
-        id:1,
-        name:"Project One",
-        title:"Project One",
-        description:"Full stack mobile responsive web app",
-        tags:['Full Stack'],
-        projectLink:"/",
-        codeLink:"/",
-        imgUrl:Img1
-    },
-    {
-        id:2,
-        name:"Project Two",
-        title:"Project Two",
-        description:"Front end mobile responsive web app",
-        tags:['Front End'],
-        projectLink:"/",
-        codeLink:"/",
-        imgUrl:Img2
-    },
-    {
-        id:3,
-        name:"Project Three",
-        title:"Project Three",
-        description:"Full stack app",
-        tags:['Full Stack'],
-        projectLink:"/",
-        codeLink:"/",
-        imgUrl:Img1
-    },
-    {
-        id:4,
-        name:"Project Four",
-        title:"Project Four",
-        description:"Full stack app",
-        tags:['Full Stack'],
-        projectLink:"/",
-        codeLink:"/",
-        imgUrl:Img2
-    }
-];
-*/
 
 export const workData = [
     {
@@ -58,7 +15,7 @@ export const workData = [
         imgUrl:Img1
     },
     {
-        id:1,
+        id:2,
         name:"Dembublog",
         title:"Dembublog",
         description:"Full stack mobile responsive blog.MongoDB, Express, React, Nodejs, Tailwind css",
@@ -66,5 +23,15 @@ export const workData = [
         projectLink:"https://dembublog.vercel.app/",
         codeLink:"https://github.com/moshudougee/dembublog",
         imgUrl:Img2
+    },
+    {
+        id:3,
+        name:"Demb Estate",
+        title:"Dembu Estate",
+        description:"Full stack mobile responsive blog.MongoDB, Express, React, Nodejs, Tailwind css",
+        tags:['Full Stack'],
+        projectLink:"https://dembu-estate.onrender.com/",
+        codeLink:"https://github.com/moshudougee/dembu-estate",
+        imgUrl:Img3
     }
 ];
