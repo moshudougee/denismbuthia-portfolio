2f0b729563bf937abfb2c83bdd63b7ab57a3153b
moshudougee <mbuthiadenis@gmail.com>
2024-05-08 13:52:53 +0300

    linkedIn link
    Signed-off-by: moshudougee <mbuthiadenis@gmail.com>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -102,7 +102,7 @@ const Navbar = () => {
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
                     <a
                     className='flex justify-between items-center w-full text-gray-300'
-                    href='https://www.linkedin.com/in/mbuthia-denis-17a56842/'
+                    href='https://www.linkedin.com/in/denis-mbuthia-17a56842/'
                     target='_blank'
                     rel='noreferrer'
                     >
